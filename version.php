9130d0cf8f52114641b897202d33d5f763b3a2dd
Samuel <sparkinzy@163.com>
2022-09-16 16:38:54 +0800

    fix(views): 新增views目录，避免optimize时报错

--- version.php
+++ version.php
@@ -1,10 +1,13 @@
 <?php
 
 return [
-    '1.0.0' => [
-        '基于 神级插件 viewerjs 做的一个封装，提升Dcat Admin 图片预览效果，实现放大、缩小、翻转、旋转等效果.',
+    '1.0.3' => [
+        '新增views目录，便于php artisan optimize'
     ],
     '1.0.2' => [
         '提升viewer的zindex为8个9,方便在modal实现图片预览'
-    ]
+    ],
+    '1.0.0' => [
+        '基于 神级插件 viewerjs 做的一个封装，提升Dcat Admin 图片预览效果，实现放大、缩小、翻转、旋转等效果.',
+    ],
 ];
